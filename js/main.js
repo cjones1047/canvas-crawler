a7fc5bbfe13efebfe6540b7754dc41ff9e0db62b
cjones1047 <cjmj1047@gmail.com>
2022-06-22 10:52:03 -0500

    New advanced game

--- js/main.js
+++ js/main.js
@@ -1,166 +1,188 @@
-// first, we need to save the canvas and the positioning message area to variables
 // this is so we can refer to them later
-
 const game = document.getElementById('canvas')
 const movement = document.getElementById('movement')
 
 // we also need to define our game context
 const ctx = game.getContext('2d')
 
-// so, we have a variable height and width on our canvas, so we need to get that height and width as a reference point so we could do stuff with it later.
-// for this, we'll use, the built in getComputedStyle function
-// we'll use this along with setAttribute
+game.setAttribute('width', getComputedStyle(game)['width'])
+game.setAttribute('height', getComputedStyle(game)['height'])
 
-game.setAttribute('width',getComputedStyle(game)['width'])
-game.setAttribute('width',getComputedStyle(game)['width'])
+console.log('this is the canvas width', game.width)
+console.log('this is the canvas height', game.height)
 
-console.log('this is the canvas width', canvas.width)
-console.log('this is the canvas height', canvas.height)
+// to instantiate instances of classes, or as we call them, objects
+// we call the class method with a very specific syntax
 
-// this is where we start using OOP principles
-// OOP = Object Oriented Programming
-
-// const someObject = {
-//     name: 'object',
-//     height: 50,
-//     favoriteFood: 'donut',
-//     alive: false,
-//     friends: ['billy','cindy','captain hook'],
-//     itemsInPocket: [
-//         { name: 'phone', size: 'small'},
-//         { name: 'lint', size: 'extra small'}
-//     ]
-// }
-
-// Sometimes, we want to make a bunch of the same object
-// for this, we can use a class
-
-// we're going to use a class to create items on our canvas
-// this way, we can write DRY code, and create elements of different shapes and sizes
-// classes ALL start with a capital letter
-// use 'class' keyword to create them
-
-// objects are made of PROPERTIES (which are key: value pairs) and METHODS (functions)
-class Crawler {
-    // classes can ALSO have (and usually do) a constructor function
-    // this is how we tell our class, exactly how we want to build our object
-    // this also allows us to use the keyword 'this' in reference to whatever object has been made by the class
-    // attributes that are variable go in the 'constructor' function
-    constructor(x, y, color, width, height) {
-        // in here, this is how I define what my object will be made of
+class OgreCrawler {
+    constructor(x, y, color, width, height, type) {
         this.x = x,
         this.y = y,
         this.color = color,
         this.height = height,
         this.width = width,
-        // anything that is going to be the same for all instances of the objects we create:
-        // we can set the hard value here (like below) and leave that out of the constructor's parameters
-        this.alive = true,
-        // we can also add methods
-        // in our case, the method is going to be 'render'
-        this.render = function () {
-            // here, we will set the fillStyle and the fillRect
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.width, this.height)
-        }
+        this.type = type,
+        this.alive = true
     }
 
+    render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
 }
 
-// to instantiate instances of classes (or as we call them, objects)
-// we call the class method with a very specific syntax
-let player = new Crawler(10, 10, 'lightsteelblue', 16, 16)
-let ogre = new Crawler(200, 50, '#bada55', 32, 48)
+// HeroCrawler will need a few additional properties in order for movement to be smooooooooooooooooth
+class HeroCrawler {
+    constructor(x, y, color, width, height) {
+        this.x = x,
+        this.y = y,
+        this.color = color,
+        this.width = width,
+        this.height = height,
+        this.alive = true,
+        // two additional properties related to smooth movement
+        this.speed = 15,
+        // smooth animation works a lil differently than what we saw before
+        // the way this animation works, is putting our character in constant motion, but freezing their position
+        // that way, we just 'decide' the direction and can unfreeze their position based on keypress input. We'll call this 'setting the direction'
+        this.direction = {
+            up: false,
+            down: false,
+            left: false,
+            right: false
+        }
+    }
+    // this is where we'll add our methods, all of these pertain to smoothing out our player's movement
+    // setDirection tells player which way to move -ex changes up to true
+    // this will be our keydown event
+    setDirection = function (key) {
+        console.log('this is the key that was pressed', key)
+        if (key.toLowerCase() == 'w') { this.direction.up = true }
+        if (key.toLowerCase() == 'a') { this.direction.left = true }
+        if (key.toLowerCase() == 's') { this.direction.down = true }
+        if (key.toLowerCase() == 'd') { this.direction.right = true }
+    }
+    // unSetDirection -> ex changes our direction back to false
+    // this will be our keyup event
+    unSetDirection = function (key) {
+        console.log('this is the key that was pressed', key)
+        if (key.toLowerCase() == 'w') { this.direction.up = false }
+        if (key.toLowerCase() == 'a') { this.direction.left = false }
+        if (key.toLowerCase() == 's') { this.direction.down = false }
+        if (key.toLowerCase() == 'd') { this.direction.right = false }
+    }
+    movePlayer = function () {
+        // move player looks at the direction, and sends the guy flying in whatever direction is true
+        if (this.direction.up) {
+            this.y -= this.speed
+            // bc we're tracking up movement, let's stop our player
+            // from exiting the top of the canvas
+            if (this.y <= 0) {
+                this.y = 0
+            }
+        }
+        if (this.direction.left) {
+            this.x -= this.speed
+            // bc we're tracking the left moves, stop him at left edge
+            if (this.x <= 0) {
+                this.x = 0
+            }
+        }
+        if (this.direction.down) {
+            this.y += this.speed
 
-// the gameLoop function will be what helps us create an animation effect
-// it also allows us to say what happens when and control those events to our liking
+            // this tracks down movement, so we need to consider the height
+            if (this.y + this.height >= game.height) {
+                this.y = game.height - this.height
+            }
+        }
+        if (this.direction.right) {
+            this.x += this.speed
+
+            // this tracks right movement, so we need to consider the width
+            if (this.x + this.width >= game.width) {
+                this.x = game.width - this.width
+            }
+        }
+    }
+    // we'll keep our render method nice and simple, and keep it at the bottom of our class.
+    render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+const randomPlaceShrekX = (max) => {
+    // we use this func to produce a random x coord for shrek 2
+    return Math.floor(Math.random() * max)
+}
+
+let player = new HeroCrawler(10, 10, 'lightsteelblue', 16, 16)
+let ogre = new OgreCrawler(200, 50, '#bada55', 32, 48, 'ogre')
+let ogreTwo = new OgreCrawler(randomPlaceShrekX(game.width), 50, 'red', 64, 96, 'big ogre')
+
+// The gameLoop function will be what helps us create an animation effect
+// it also allows us to say what happens when and control those events to our liking.
 // this is how we utilize movement
 
 const gameLoop = () => {
-    // the 'win' condition of our game is to kill Shrek
-    // so if our ogre is alive, then the game can continue
-    // if the ogre is deceased, the game will end
-    if(ogre.alive) {
-        detectHit()
-    }
-    // we need to render both of our objects, and we'll use their respective render methods to do this
-    // we will also, update our movement box with the coordinatews of our player
-    // and to create the illusion of movement, we need to clear the canvas every single 'frame' so that our hero's movement doesn't turn him into a snake
-   
-    ctx.clearRect(0, 0, game.width, game.height);
-    movement.textContent = player.x + ', ' + player.y;
-    player.render()
-
-    if(ogre.alive) {
+    ctx.clearRect(0, 0, game.width, game.height)
+    
+    movement.textContent = player.x + ', ' + player.y
+    
+    // if ogre is alive, render and detect hit
+    // if first ogre croaks, render the next one
+    if (ogre.alive) {
         ogre.render()
+        detectHit(ogre)
+    } else if (ogreTwo.alive) {
+        document.querySelector('#status').textContent = 'Now Kill Shrek 2!'
+        ogreTwo.render()
+        detectHit(ogreTwo)
+    } else {
+        stopGameLoop()
+        document.querySelector('#status').textContent = 'You Win!'
     }
+
+    player.render()
+    player.movePlayer()
 }
 
+let gameInterval = setInterval(gameLoop, 60)
+
+const stopGameLoop = () => {clearInterval(gameInterval)}
 // we're going to use setInterval to repeat our game loop function at specific times
 // we're going to do this, when the content loads
 document.addEventListener('DOMContentLoaded', function () {
-    // in here, we need to have our movement handler
-    document.addEventListener(('keydown'), movementHandler)
-    // we also need our game loop running at an interval
-    setInterval(gameLoop, 60)
+    // this starts the game when the content is loaded
+    gameInterval
 })
 
-// this function is going to be how we move our player around
-// we'll use 'e' to take the place of an event
+// two new event handlers are needed, one for keyup and one for keydown
+document.addEventListener('keydown', (e) => {
+    // when the key is down, set the direction according to our
+    // player.setDirection method
+    player.setDirection(e.key)
+})
 
-const movementHandler = (e) => {
-    // this movement handler is going to be attached to an event listener
-    // we'll attach it with a keydown event
-    // key events can use the key itself or a 'KEYCODE'
-    // we'll use 'keycodes'
-        // w = 87, a = 65, s = 83, d = 68
-        // up = 38, down = 40, left = 37, right = 39
-    // in order to do different things for different keys, we can use 'if' statements 
-    // OR 
-    // we can use a 'switch' case
-    // switch cases are handy when you have multiple possibilities
-    // switch case has a main switch, and cases(in this our cases are inputs)
-    // we also need a break in every case, so we can read this multiple times
-    switch (e.keyCode) {
-        // adding cases without 'break' in between allows multiple cases to be used to produce the same functions
-        case (87):
-        case (38):
-            // this moves player up
-            player.y -= 10
-            // we also need to break the case
-            break
-        case (65):
-        case (37):
-            // this moves player left
-            player.x -= 10
-            break
-        case (83):
-        case (40):
-            // this will move the player down
-            player.y += 10
-            break
-        case (68):
-        case (39):
-            // this moves the player to the right
-            player.x += 10
-            break
+document.addEventListener('keyup', (e) => {
+    // this one will look a lil different than keydown
+    // we need to make sure it only applies to the keys we listed in unSetDirection
+    if (['w', 'a', 's', 'd'].includes(e.key)) {
+        player.unSetDirection(e.key)
     }
+})
 
 
-}
-
-// to detect a collision between objects
-// we need to account for the entire space that the object takes up
-// here, we'll write a collisison detection function that
-// takes our dimensions into account, and says what to do if the collision happens
-const detectHit = () => {
-    // we'll use one one big if statement that clearly defines any moment of collision
+// to detect a collision between different objects
+// we can give our function a parameter, and pass an argument every time it's called
+const detectHit = (thing) => {
+    // we'll use one big if statement that clearly defines any moment of collision.
     // that means utilizing, x, y, width and height of our objects
-    if (player.x < ogre.x + ogre.width
-        && player.x + player.width > ogre.x
-        && player.y < ogre.y + ogre.height
-        && player.y + player.height > ogre.y) {
-            ogre.alive = false
-            document.getElementById('status').textContent = 'You win!'
+    if (player.x < thing.x + thing.width
+        && player.x + player.width > thing.x
+        && player.y < thing.y + thing.height
+        && player.y + player.height > thing.y) {
+            thing.alive = false
         }
 }
